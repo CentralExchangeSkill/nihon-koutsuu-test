3a1d1473269550bbfd696ecb34ec9106fc0107d2
unknown <widianto@PCSTCE22.sss.seino.co.jp>
2026-05-08 09:25:54 +0900

    update 1.1.3 revision nepali language

--- data/update-id.js
+++ data/update-id.js
@@ -4,13 +4,13 @@ const data = JSON.parse(fs.readFileSync("50q_3.json", "utf8"));
 
 const updatedData = data.map((item, index) => ({
   ...item,
-  id: index + 101
+  id: index + 1
 }));
 
 fs.writeFileSync(
-  "50q_3_updated.json",
+  "50q_3.json",
   JSON.stringify(updatedData, null, 2),
   "utf8"
 );
 
-console.log("ID berhasil diubah dari 101 sampai 150");
+console.log("ID berhasil diubah");
